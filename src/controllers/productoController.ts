09b031b2c8f113f832f4503225bf60b28d96de22
yonier202 <yonier202@gmail.com>
2021-11-16 22:40:17 -0500

    Backend listo

--- src/controllers/productoController.ts
+++ src/controllers/productoController.ts
@@ -1,7 +1,7 @@
 import executeQuery from "../services/mysql.service";
 
 const obtenerProductos = async (req, res, next) => {
-    await executeQuery('SELECT * FROM super_tienda').then((response)=>{
+    await executeQuery('SELECT * FROM producto').then((response)=>{
         const data = {
             message: `${response.length} datos encontrados`,
             data: response.length > 0 ? response : null
@@ -15,7 +15,7 @@ const obtenerProductos = async (req, res, next) => {
 
 const obtenerProducto = async(req, res, next) => {
     try{
-        const response = await executeQuery(`SELECT * FROM super_tienda WHERE idproducto = ${req.params.id} `);
+        const response = await executeQuery(`SELECT * FROM producto WHERE idproducto = ${req.params.id} `);
         res.send(response);
     }catch(error){
         next(error);
@@ -25,7 +25,7 @@ const obtenerProducto = async(req, res, next) => {
 
 const agregarProducto = async(req, res, next) => {
     try{
-        const response = await executeQuery (`INSERT INTO super_tienda (nombre, Referencia, cantidad, valor, descripcion) VALUES ('${req.body.nombre}', '${req.body.Referencia}', '${req.body.cantidad}', '${req.body.valor}', '${req.body.descripcion}',)`)
+        const response = await executeQuery (`INSERT INTO producto (nombre, Referencia, cantidad, valor, descripcion) VALUES ('${req.body.nombre}', '${req.body.Referencia}', '${req.body.cantidad}', '${req.body.valor}', '${req.body.descripcion}')`)
         res.status(201).json({ message: 'created', id: response.insertId});
     }catch(error){
         next(error);
@@ -35,8 +35,8 @@ const agregarProducto = async(req, res, next) => {
 const actualizarProducto = (req, res, next) => {
     const {nombre, Referencia, cantidad, valor, descripcion} = req.body;
     const {id}=req.params;
-    executeQuery(`UPDATE super_tienda SET nombre = '${nombre}', Referencia = '${Referencia}', cantidad = '${cantidad}', valor = '${valor}', descripcion = '${descripcion}' WHERE idproducto = '${id}'`).then((response)=>{
-    response.json({message: response.affectedRows > 0 ? 'Updated' : 'No hay registro con este Id'});
+    executeQuery(`UPDATE producto SET nombre = '${req.body.nombre}', Referencia = '${req.body.Referencia}', cantidad = '${req.body.cantidad}', valor = '${req.body.valor}', descripcion = '${req.body.descripcion}' WHERE idproducto = '${id}'`).then((response)=>{
+    res.json({message: response.affectedRows > 0 ? 'Updated' : 'No hay registro con este Id'});
     }).catch((error)=>{
         next(error);
     })
@@ -44,8 +44,7 @@ const actualizarProducto = (req, res, next) => {
 }
 
 const eliminarProducto = (req, res, next) => {
-    executeQuery(`DELETE * FROM super_tienda WHERE idproducto = ${req.params.id} `).then((response) =>{
-        console.log(response);
+    executeQuery(`DELETE FROM producto WHERE idproducto = ${req.params.id}`).then((response) =>{
         res.json({message: response.affectedRows > 0 ? 'deleted' : 'No hay registro con este Id'});
     }).catch((error)=>{
         next(error);
