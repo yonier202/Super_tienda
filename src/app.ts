6954631ddf285b858669233fa8600d374d518733
yonier202 <yonier202@gmail.com>
2021-11-22 09:30:06 -0500

    proyecto backend  finalizado.

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+app.use((_, res, next) =>{
+    res.header('Acess-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', '*');
+    res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 producto(app);
 app.get('/prueba/:id', async (req, res, next) => {
     
